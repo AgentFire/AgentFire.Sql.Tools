dc7ec3bafc7e181c11a9cd770d125e1fa685845c
AgentFire <agentfire@mail.ru>
2017-12-27 14:08:37 +0300

    1.1.1

--- Sql Tools/ICrud.cs
+++ Sql Tools/ICrud.cs
@@ -1,12 +1,18 @@
-﻿using System.Data.Linq;
+﻿using System;
+using System.Data.Linq;
+using System.Linq.Expressions;
 
 namespace AgentFire.Sql.Tools
 {
     public interface ICrud<TDbContext> where TDbContext : DataContext
     {
-        bool TryDelete<T>(int id) where T : class;
-        int Create<T>(EntityAction<TDbContext, T> initializer) where T : class, new();
-        bool Modify<T>(int id, EntityAction<TDbContext, T> modifier) where T : class;
         T Get<T>(int id) where T : class;
+        int Create<T>(EntityAction<TDbContext, T> initializer) where T : class, new();
+
+        bool TryDelete<T>(int id) where T : class;
+        int TryDelete<T>(Expression<Func<T, bool>> predicateExpression) where T : class;
+
+        bool Modify<T>(int id, EntityAction<TDbContext, T> modifier) where T : class;
+        int Modify<T>(Expression<Func<T, bool>> predicateExpression, EntityAction<TDbContext, T> modifier) where T : class;
     }
 }
